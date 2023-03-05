5e4596535fae52918ba7abec18685afe3e67dfe9
frunolu <fruno.lu@gmail.com>
2023-03-05 15:05:46 +0100

    Edit Bootrap.php

--- project/app/Bootstrap.php
+++ project/app/Bootstrap.php
@@ -1,30 +1,62 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App;
 
+use Nette\Bootstrap\Configurator;
 use Nette\DI\Container;
 use Nette\DI\ContainerLoader;
 use RuntimeException;
 
 class Bootstrap
 {
-    public static function boot(): Container
+//declare(strict_types=1);
+//
+//namespace App;
+//
+//use Nette\Bootstrap\Configurator;
+//use Nette\DI\Container;
+//use Nette\DI\ContainerLoader;
+//use RuntimeException;
+//
+//class Bootstrap
+//{
+//    public static function boot(): Container
+//    {
+//        $appDir = dirname(__DIR__);
+//        $loader = new ContainerLoader($appDir . '/temp', true);
+//        $class = $loader->load(function ($compiler) use ($appDir) {
+//            $compiler->loadConfig($appDir . '/config/config.neon');
+//        });
+//
+//
+//        $container = new $class();
+//        if ($container instanceof Container === false) {
+//            throw new RuntimeException('Unable to create DI container.');
+//        }
+//
+//        return $container;
+//    }
+
+
+    public static function boot(): Configurator
     {
+        $configurator = new Configurator;
         $appDir = dirname(__DIR__);
-        $loader = new ContainerLoader($appDir . '/temp', true);
-        $class = $loader->load(function ($compiler) use ($appDir) {
-            $compiler->loadConfig($appDir . '/config/config.neon');
-        });
 
+        //$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+        $configurator->setDebugMode(true);
 
-        $container = new $class();
-        if ($container instanceof Container === false) {
-            throw new RuntimeException('Unable to create DI container.');
-        }
+        $configurator->setTempDirectory($appDir . '/temp');
 
-        return $container;
+        $configurator->createRobotLoader()
+            ->addDirectory(__DIR__)
+            ->register();
+
+        $configurator->addConfig($appDir . '/config/config.neon');
+        $configurator->addConfig($appDir . '/config/local.neon');
+
+        return $configurator;
     }
 }
 
